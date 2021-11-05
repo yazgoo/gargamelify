91914e40492e45af23a2d992d86a1bc21e2a64e0
yazgoo <yazgoo@gmail.com>
2021-11-05 11:29:00 +0100

    support twitter

--- gargamelify.js
+++ gargamelify.js
@@ -1,6 +1,8 @@
-var textNode;
-let walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
+const targetNode = document.body;
 
+function gargamelify(item) {
+var textNode;
+let walk = document.createTreeWalker(item, NodeFilter.SHOW_TEXT, null, false);
 // Replace in body
 while (textNode = walk.nextNode()) {
     textNode.nodeValue = textNode.nodeValue
@@ -9,3 +11,17 @@ while (textNode = walk.nextNode()) {
   .replace(/([EÉeé]ric *)?[Zz]emmour/g, 'Gargamel');
 }
 
+}
+
+const config = { childList: true, subtree: true };
+const callback = function(mutationsList, observer) {
+    for(let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          gargamelify(mutation.target)
+        }
+    }
+};
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
+
+gargamelify(targetNode)
