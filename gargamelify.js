8fb54410bc30791cc9a980cb15cc05f8865bf2b3
yazgoo <yazgoo@gmail.com>
2021-11-05 15:27:57 +0100

    indent

--- gargamelify.js
+++ gargamelify.js
@@ -1,25 +1,25 @@
 const targetNode = document.body;
 
 function gargamelify(item) {
-var textNode;
-let walk = document.createTreeWalker(item, NodeFilter.SHOW_TEXT, null, false);
-// Replace in body
-while (textNode = walk.nextNode()) {
+  var textNode;
+  let walk = document.createTreeWalker(item, NodeFilter.SHOW_TEXT, null, false);
+  // Replace in body
+  while (textNode = walk.nextNode()) {
     textNode.nodeValue = textNode.nodeValue
-  .replace(/d'([EÉeé]ric *)?[Zz]emmour/g, 'de Gargamel')
-  .replace(/qu'([EÉeé]ric *)?[Zz]emmour/g, 'que Gargamel')
-  .replace(/([EÉeé]ric *)?[Zz]emmour/g, 'Gargamel');
-}
+      .replace(/d'([EÉeé]ric *)?[Zz]emmour/g, 'de Gargamel')
+        .replace(/qu'([EÉeé]ric *)?[Zz]emmour/g, 'que Gargamel')
+          .replace(/([EÉeé]ric *)?[Zz]emmour/g, 'Gargamel');
+        }
 
 }
 
 const config = { childList: true, subtree: true };
 const callback = function(mutationsList, observer) {
-    for(let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-          gargamelify(mutation.target)
-        }
+  for(let mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      gargamelify(mutation.target)
     }
+  }
 };
 const observer = new MutationObserver(callback);
 observer.observe(targetNode, config);
